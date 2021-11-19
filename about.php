5783177abf4c00d611bf06b50fa2c5170863f722
zaireeeen <93666537+zaireeeen@users.noreply.github.com>
2021-11-19 22:10:09 +0800

    added aboutpage

--- about.php
+++ about.php
@@ -20,29 +20,38 @@
         </h3>
 
         <div class = 'left'>
-            <h4 class = 'white'>
+            <h4 class = 'heading'>
                 WHO ARE WE?
             </h4>
             <p class = 'left'> Well, to sum everything up, we are four broke
                 college students with the IQ level of 500, who are trying to earn money by selling... "plants". <br>
-                The geniuses in this group is Sailendran, Darrel, Zaireen and Abdul. We created this online shop with our blood, sweat and tears. <br>
-                Let's get to know the website more.
+                The geniuses in this group is Sailendran, Darrel, Zaireen and Abdul. We created this online shop with our blood, sweat and tears.  Let's get to know the website more.
             </p>
             
         </div>
         <div class = 'left'>
-            <h4 class = 'white'>
+            <h4 class = 'heading'>
                 WEBSITE FEATURES
             </h4>
-            <p class = 'white'> First of all, let's just list down the features we have 
-</p>
-            <ul>
-                <li> A login and signup page </li>
-                <li> Dynamic display for products </li>
-                <li> Dynamic page for each products </li>
-                <li> A fake payment page </li>
-                <li> Cart system </li>
-            
+            <p class = 'heading'> What makes our website so special? Here's a list of the features we have: <br>
+            </p>
+            <ol>
+                <li class='list'> Login and Signup page </li>
+                <li class='list'> Dynamic display for products </li>
+                <li class='list'> Dynamic page for each product </li>
+                <li class='list'> A fake payment page </li>
+                <li class='list'> A cart system </li>
+                <li class='list'> Authorized access for users and admin </li>
+            </ol>
+            <p> As you can see, we have a fully functional login and signup page where you can create an account and also sign in to access your cart! <br>
+            The cart system is protected, therefore no one can see what you're planning to buy *wink*... SO don't worry, your secret is safe with us, you weirdo. <br>
+            Dynamic pages are also created for you to view the different products that we offer! It's kinda cool.
+        
+        
+        </p>
+
+        
+
         </div>
 
     </div>
